46140b2c421093909ccfbb052d9aa844a9eadb11
Wheber Silvestre <whebersilvestre4@gmail.com>
2026-05-23 07:23:30 +0100

    Production backend API

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-const API_BASE =
-  import.meta.env.VITE_API_URL ||
-  'http://localhost:5000/api';
-
 const api = axios.create({
-  baseURL: 'http://172.20.10.2:5000/api',
+  baseURL:
+    'https://loadflow-backend-6idm.onrender.com',
 });
 
 export default api;
